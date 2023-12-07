ef409197f6bb55de5ab0b113deb5a277694177ce
Aartt! <151869774+ebarthur@users.noreply.github.com>
2023-12-07 12:32:04 +0000

    Update Dashboard.jsx
    The added feature displays all the events the user registered for.

--- client/meetup-clone/src/pages/Dashboard.jsx
+++ client/meetup-clone/src/pages/Dashboard.jsx
@@ -19,3 +19,32 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+app.post("/user/events", (req, res) => {
+    const { userID } = req.body;
+    let userEvents = [];
+    for (let i = 0; i < events.length; i++) {
+        let result = events[i].attendees.filter((user) => user === userID);
+        if (result.length > 0) {
+            userEvents.push(events[i]);
+        }
+    }
+    res.json({ message: "Successful", events: userEvents });
+});
+const fetchMyEvents = (userID, setEvents) => {
+    fetch("http://localhost:4000/user/events", {
+        method: "POST",
+        body: JSON.stringify({ userID }),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message) {
+                console.log(data);
+                setEvents(data.events);
+            }
+        })
+        .catch((err) => console.error(err));
+};
