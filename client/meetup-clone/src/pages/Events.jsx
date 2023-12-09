0f0b45468e15676060aa0773a62157283f082a8f
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-09 21:05:57 +0000

    Update Home.jsx
    I moved some codes from Home.jsx into Events.jsx where it should belong

--- client/meetup-clone/src/pages/Events.jsx
+++ client/meetup-clone/src/pages/Events.jsx
@@ -1,3 +1,69 @@
+import event from "../assets/event.jpeg";
+import Nav from "../components/Nav";
+import { AiOutlineCalendar } from "react-icons/ai";
+import { BsCheckCircle } from "react-icons/bs";
+import { ImLocation2 } from "react-icons/im";
+import { Link } from "react-router-dom";
+
+const Events = () => {
+    return (
+        <>
+            <Nav />
+            <div className='home_events' style={{ paddingTop: "20px" }}>
+                <h1 style={{ fontSize: "30px", marginBottom: "20px" }}>All Events</h1>
+
+                <div className='body_events'>
+                    <Link to={`/event/slug`} className='i_event'>
+                        <img src={event} alt='Event' className='i_image' />
+                        <div className='i_content'>
+                            <h2 style={{ marginBottom: "10px" }}>Novu Community Call</h2>
+                            <p style={{ marginBottom: "10px", opacity: 0.7 }}>
+                                Hosted by: Novu Development Team
+                            </p>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    opacity: 0.7,
+                                    marginBottom: "10px",
+                                }}
+                            >
+                                <AiOutlineCalendar style={{ marginRight: "5px" }} />
+                                <p>Starting at 8:00pm</p>
+                            </div>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    opacity: 0.7,
+                                    marginBottom: "10px",
+                                }}
+                            >
+                                <ImLocation2 style={{ marginRight: "5px", color: "red" }} />
+                                <p>Online (Discord Channel)</p>
+                            </div>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    opacity: 0.7,
+                                    marginBottom: "10px",
+                                }}
+                            >
+                                <BsCheckCircle style={{ marginRight: "5px", color: "green" }} />
+                                <p>12 going</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Events;
+
+
 const createSlug = (text) => {
     let slug = text
         .trim()
@@ -117,6 +183,7 @@ const postNewComment = (comment, user, slug) => {
         }
     }
 });
+
 const eventRegister = (user, id) => {
     fetch("http://localhost:4000/register/event", {
         method: "POST",
